ee83215c73a3656edd32e7adce73fb63c6b43907
Michal <mincoslav@gmail.com>
2022-04-06 09:22:28 +0200

    changed from async to promises

--- authentication.ts
+++ authentication.ts
@@ -6,14 +6,20 @@ async function save(key: string, value: string) {
 	await SecureStore.setItemAsync(key, value);
 }
 
-async function getValueFor(key: string) {
-	let result = await SecureStore.getItemAsync(key);
-	if (result) {
-		alert("🔐 Here's your value 🔐 \n" + result);
-		return result;
-	} else {
-		alert("No values stored under that key.");
-	}
+function getValueFor(key: string): any {
+	SecureStore.getItemAsync(key).then(
+		(result) => {
+			if (result) {
+				alert("🔐 Here's your value 🔐 \n" + result);
+				return result;
+			} else {
+				alert("No values stored under that key.");
+				return false
+			}
+		}
+	).catch((error)=> {
+		return false
+	})
 }
 // request, response, promptAsync,
 export { save, getValueFor}
