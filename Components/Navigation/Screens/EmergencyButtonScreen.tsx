0575ce6f66875a980f64fa89c2156f510cd13d25
etlimss <pavilionytesmilte@gmail.com>
2022-04-13 19:35:33 +0200

    Working popup as core functionaly, needs polishing

--- Components/Navigation/Screens/EmergencyButtonScreen.tsx
+++ Components/Navigation/Screens/EmergencyButtonScreen.tsx
@@ -1,21 +1,29 @@
-import React from "react";
-import { View, StyleSheet, Dimensions, Pressable, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Text,
+  Alert,
+  Modal,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import EmergencyButtonUnpressed from "../../../assets/svgs/emergencyPage/EmergencyButtonUnpressed";
 
+import EmergencyButton from "../../EmergencyButton";
 export const windowWidth = Dimensions.get("window").width;
 export const windowHeight = Dimensions.get("window").height;
 
 export const mainGradient = ["rgba(52, 170, 252, 1)", "rgba(118, 10, 202, 1)"];
 
+
+
 export default function EmergencyButtonPage() {
   return (
     <View style={styles.container}>
-      <LinearGradient colors={mainGradient} style={styles.background}>
-        <Pressable style={styles.emergencyButton}>
-          <EmergencyButtonUnpressed />
-        </Pressable>
-      </LinearGradient>
+      
+      <EmergencyButton />
     </View>
   );
 }
@@ -47,5 +55,46 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "column",
     elevation: 2,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
