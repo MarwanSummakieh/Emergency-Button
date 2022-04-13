2f178856e9110519b160b859e48031c6cc3b612b
Marwan Summakieh <marwansummakieh97@gmail.com>
2022-04-13 12:06:09 +0200

    the polyobject isn't recognised for some reason

--- Components/MapViewComponent.tsx
+++ Components/MapViewComponent.tsx
@@ -2,10 +2,9 @@ import { View } from "react-native";
 import React, { useEffect } from "react";
 import { mainGradient, styles } from "../css/styles";
 import { LinearGradient } from "expo-linear-gradient";
-import MapView, { Circle, Marker } from "react-native-maps";
+import MapView, { Circle, Marker, Polygon } from "react-native-maps";
 import * as Location from "expo-location";
 import d3, { geoContains } from "d3-geo";
-import { Polygon } from "react-native-svg";
 export default function MapViewComponent() {
   const [latitude, setLatitude] = React.useState(0);
   const [longitude, setLongitude] = React.useState(0);
@@ -17,49 +16,51 @@ export default function MapViewComponent() {
   };
 
   //the geoLocation object obtained from geoJson
-  const dangerousArea = 
-    {
-      "type": "FeatureCollection",
-      "features": [
-        {
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "Polygon",
-            "coordinates": [
+  const dangerousArea = [
+    { latitude: 12.538511753082275, longitude: 55.8107594520295 },
+    { latitude: 12.538619041442871, longitude: 55.8138340359014 },
+    { latitude: 12.533115148544312, longitude: 55.81333970742995 },
+    { latitude: 12.534842491149902, longitude: 55.810946345433216 },
+    { latitude: 12.538511753082275, longitude: 55.8107594520295 },
+  ];
+  const polyObject =
+  {
+
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [
+            [
               [
-                [
-                  12.538511753082275,
-                  55.8107594520295
-                ],
-                [
-                  12.538619041442871,
-                  55.8138340359014
-                ],
-                [
-                  12.533115148544312,
-                  55.81333970742995
-                ],
-                [
-                  12.534842491149902,
-                  55.810946345433216
-                ],
-                [
-                  12.538511753082275,
-                  55.8107594520295
-                ]
+                12.538511753082275,
+                55.8107594520295
+              ],
+              [
+                12.538619041442871,
+                55.8138340359014
+              ],
+              [
+                12.533115148544312,
+                55.81333970742995
+              ],
+              [
+                12.534842491149902,
+                55.810946345433216
+              ],
+              [
+                12.538511753082275,
+                55.8107594520295
               ]
             ]
-          }
+          ]
         }
-      ]
-    };
-  const dangerousCircle = {
-    latitude: 55.86,
-    longitude: 9.84,
-    radius: 100,
-  };
-
+      }
+    ]
+  }
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -70,11 +71,10 @@ export default function MapViewComponent() {
       let location = await Location.getCurrentPositionAsync({});
       setLatitude(location.coords.latitude);
       setLongitude(location.coords.longitude);
-//need to check if the location is within the geoJson polygon
-      if(d3.geoContains(dangerousCircle, [longitude, latitude])){
+      //need to check if the location is within the geoJson polygon
+      if (d3.geoContains(polyObject, [longitude, latitude])) {
         console.log("inside");
       }
-     
     })();
   }, []);
 
@@ -84,14 +84,8 @@ export default function MapViewComponent() {
         <View style={styles.container}>
           <MapView style={styles.map} showsUserLocation={true} region={region}>
             <Polygon
-              points={[
-                [12.538511753082275, 55.8107594520295],
-                [12.538619041442871, 55.8138340359014],
-                [12.533115148544312, 55.81333970742995],
-                [12.534842491149902, 55.810946345433216],
-                [12.538511753082275, 55.8107594520295],
-              ]}
-            ></Polygon>
+              coordinates={dangerousArea}
+              />
           </MapView>
         </View>
       </LinearGradient>
