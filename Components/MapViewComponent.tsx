677703216fc658705f6cf38ff3fa7da5add4794a
Marwan Summakieh <marwansummakieh97@gmail.com>
2022-04-13 09:13:28 +0200

    MapViewComponent-line74&87-93

--- Components/MapViewComponent.tsx
+++ Components/MapViewComponent.tsx
@@ -1,54 +1,100 @@
 import { View } from "react-native";
 import React, { useEffect } from "react";
-import { mainGradient, styles} from "../css/styles";
+import { mainGradient, styles } from "../css/styles";
 import { LinearGradient } from "expo-linear-gradient";
-import MapView, { Marker } from "react-native-maps";
-import * as Location from 'expo-location';
-
+import MapView, { Circle, Marker } from "react-native-maps";
+import * as Location from "expo-location";
+import d3, { geoContains } from "d3-geo";
+import { Polygon } from "react-native-svg";
 export default function MapViewComponent() {
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+  const region = {
+    latitude: latitude,
+    longitude: longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
-	const [latitude, setLatitude] = React.useState(0);
-	const [longitude, setLongitude] = React.useState(0);
-	const region = {
-		latitude: latitude,
-		longitude: longitude,
-		latitudeDelta: 0.0922,
-		longitudeDelta: 0.0421,
-	};
+  //the geoLocation object obtained from geoJson
+  const dangerousArea = 
+    {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "properties": {},
+          "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+              [
+                [
+                  12.538511753082275,
+                  55.8107594520295
+                ],
+                [
+                  12.538619041442871,
+                  55.8138340359014
+                ],
+                [
+                  12.533115148544312,
+                  55.81333970742995
+                ],
+                [
+                  12.534842491149902,
+                  55.810946345433216
+                ],
+                [
+                  12.538511753082275,
+                  55.8107594520295
+                ]
+              ]
+            ]
+          }
+        }
+      ]
+    };
+  const dangerousCircle = {
+    latitude: 55.86,
+    longitude: 9.84,
+    radius: 100,
+  };
 
-	useEffect(() => {
-		(async () => {
-		  let { status } = await Location.requestForegroundPermissionsAsync();
-		  if (status !== 'granted') {
-			alert('Permission to access location was denied');
-			return;
-		  }
-	
-		  let location = await Location.getCurrentPositionAsync({});
-		  setLatitude(location.coords.latitude);
-		  setLongitude(location.coords.longitude);
-		  if (location  ) {
-			console.log(location);
-		  }
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        alert("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+//need to check if the location is within the geoJson polygon
+      if(d3.geoContains(dangerousCircle, [longitude, latitude])){
+        console.log("inside");
+      }
+     
+    })();
+  }, []);
 
-		})();
-	  }, []);
-
-	return (
-		<View style={styles.container}>
-			<LinearGradient
-				colors={mainGradient}
-				style={styles.background}
-			>
-				<View style={styles.container}>
-                    <MapView
-						style={styles.map}
-						showsUserLocation={true}
-						region={region}
-					>
-					</MapView>					
-                </View>
-			</LinearGradient>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <LinearGradient colors={mainGradient} style={styles.background}>
+        <View style={styles.container}>
+          <MapView style={styles.map} showsUserLocation={true} region={region}>
+            <Polygon
+              points={[
+                [12.538511753082275, 55.8107594520295],
+                [12.538619041442871, 55.8138340359014],
+                [12.533115148544312, 55.81333970742995],
+                [12.534842491149902, 55.810946345433216],
+                [12.538511753082275, 55.8107594520295],
+              ]}
+            ></Polygon>
+          </MapView>
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
--- Components/Navigation/Screens/EmergencyButtonScreen.tsx
+++ Components/Navigation/Screens/EmergencyButtonScreen.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet, Dimensions, Pressable, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import EmergencyButtonUnpressed from "../../../assets/svgs/emergencyPage/EmergencyButtonUnpressed";
+import * as Location from "expo-location";
 
 export const windowWidth = Dimensions.get("window").width;
 export const windowHeight = Dimensions.get("window").height;
@@ -9,43 +10,71 @@ export const windowHeight = Dimensions.get("window").height;
 export const mainGradient = ["rgba(52, 170, 252, 1)", "rgba(118, 10, 202, 1)"];
 
 export default function EmergencyButtonPage() {
-  return (
-    <View style={styles.container}>
-      <LinearGradient colors={mainGradient} style={styles.background}>
-        <Pressable style={styles.emergencyButton}>
-          <EmergencyButtonUnpressed />
-        </Pressable>
-      </LinearGradient>
-    </View>
-  );
-}
+  const [latitude, setLatitude] = React.useState(0);
+  const [longitude, setLongitude] = React.useState(0);
+  const sendinglocation = () => {
+    fetch("/post/data/here", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        UID: "123",
+        latitude: latitude,
+        longitude: longitude,
+      }),
+    }).then((res) => {
+      console.log("Request complete! response:", res);
+    });
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          alert("Permission to access location was denied");
+          return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+      })();
+    }, []);
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  background: {
-    height: windowHeight,
-    width: windowWidth,
-  },
-  text: {
-    color: "#000000",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  emergencyButton: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-  emergencyButtonView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    elevation: 2,
-  },
-});
+    return (
+      <View style={styles.container}>
+        <LinearGradient colors={mainGradient} style={styles.background}>
+          <Pressable style={styles.emergencyButton} onPress={sendinglocation}>
+            <EmergencyButtonUnpressed />
+          </Pressable>
+        </LinearGradient>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    background: {
+      height: windowHeight,
+      width: windowWidth,
+    },
+    text: {
+      color: "#000000",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    emergencyButton: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+    },
+    emergencyButtonView: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      elevation: 2,
+    },
+  });
+}
