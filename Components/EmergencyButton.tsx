5ab8d14c04d8589cd3d87b0ba20251cf8913dc40
Marwan Summakieh <marwansummakieh97@gmail.com>
2022-04-18 18:01:27 +0200

    the button only sends if the action isn't canceled

--- Components/EmergencyButton.tsx
+++ Components/EmergencyButton.tsx
@@ -11,7 +11,7 @@ import {
   Platform,
 } from "react-native";
 import * as Location from "expo-location";
-import * as SecureStore from 'expo-secure-store';
+import * as SecureStore from "expo-secure-store";
 import { LinearGradient } from "expo-linear-gradient";
 import EmergencyButtonUnpressed from "../assets/svgs/emergencyPage/EmergencyButtonUnpressed";
 import EmergencyButtonPressed from "../assets/svgs/emergencyPage/EmergencyButtonPressed";
@@ -30,245 +30,267 @@ async function messagesentNotification() {
     content: {
       title: "your message has been sent",
       body: "we'll notify you when someone responds",
-      data: { data: 'goes here' },
+      data: { data: "goes here" },
     },
-    trigger: null
+    trigger: null,
   });
 }
 
 export default function EmergencyButton() {
   const [modalVisible, setModalVisible] = useState(false);
+  const [cancel, setCancel] = useState(false);
   const uid = SecureStore.getItemAsync(userID);
-  console.log(uid);
   const [latitude, setLatitude] = React.useState(0);
   const [longitude, setLongitude] = React.useState(0);
-  const sendinglocation = () => {
-    fetch("/post/data/here", {
+  const location = {
+    type: "Point",
+    coordinates: [longitude, latitude],
+    last_updated: Date.now(),
+    country: "denamrk",
+  };
+  const conenctToSendLocation = () => {
+    fetch("http://localhost:7071/create_alert/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        UID: uid,
-        latitude: latitude,
-        longitude: longitude,
+        location: location,
+        userID: uid,
+        responderID: "",
+        resolved: false,
+        status: "received",
       }),
-    }).then((res) => {
-      if(res.status === 200){
-        messagesentNotification();
-      }
-    });
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          console.log(res);
+          messagesentNotification();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-
-
-  const showModalTimer = () => {
-    setModalVisible(true);
+  const sendLocation = () => {
     setTimeout(() => {
-      setModalVisible(false);
-    }, 4000);
-  };
-
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        alert("Permission to access location was denied");
-        return;
+      if (cancel === false) {
+        console.log("sending location");
+        conenctToSendLocation();
       }
-      let location = await Location.getCurrentPositionAsync({});
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
-    })();
-  }, []);
-  // const renderTime = ({remainingTime}) => {
-  //   if (remainingTime === 0) {
-  //     return <div className="timer">Too lale...</div>;
-  //   }
+    }, 4000);
 
-  //   return (
-  //     <div className="timer">
-  //       <div className="text">Remaining</div>
-  //       <div className="value">{remainingTime}</div>
-  //       <div className="text">seconds</div>
-  //     </div>
-  //   );
-  // };
+    const showModalTimer = () => {
+      setModalVisible(true);
+      setTimeout(() => {
+        setModalVisible(false);
+      }, 4000);
+    };
 
-  // const renderTime = ({remainingTime}) => {
-  //   CountdownCircleTimer: remainingTime === 0 ? <Text>Complete</Text>
-  // }
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          alert("Permission to access location was denied");
+          return;
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setLatitude(location.coords.latitude);
+        setLongitude(location.coords.longitude);
+      })();
+    }, []);
+    // const renderTime = ({remainingTime}) => {
+    //   if (remainingTime === 0) {
+    //     return <div className="timer">Too lale...</div>;
+    //   }
 
-  return (
-    <View style={styles.container}>
-      <LinearGradient colors={mainGradient} style={styles.background}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Emergency message was cancelled");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <CountdownCircleTimer
-                isPlaying
-                duration={3}
-                colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-                colorsTime={[3, 2, 1, 0]}
-                onComplete={() => ({ shouldRepeat: false })}
-              >
-                {({ remainingTime }) => (
-                  <Animated.Text style={{ fontSize: 50 }}>
-                    {remainingTime}
-                  </Animated.Text>
-                )}
-              </CountdownCircleTimer>
-              <Text style={styles.modalText}>
-                Emergency message is sending...
-              </Text>
+    //   return (
+    //     <div className="timer">
+    //       <div className="text">Remaining</div>
+    //       <div className="value">{remainingTime}</div>
+    //       <div className="text">seconds</div>
+    //     </div>
+    //   );
+    // };
 
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}
-              >
-                <Text style={styles.textStyle}>Cancel</Text>
-              </Pressable>
-              <Text>If you're not sure click CANCEL</Text>
+    // const renderTime = ({remainingTime}) => {
+    //   CountdownCircleTimer: remainingTime === 0 ? <Text>Complete</Text>
+    // }
+
+    return (
+      <View style={styles.container}>
+        <LinearGradient colors={mainGradient} style={styles.background}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Emergency message was cancelled");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <CountdownCircleTimer
+                  isPlaying
+                  duration={3}
+                  colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+                  colorsTime={[3, 2, 1, 0]}
+                  onComplete={() => ({ shouldRepeat: false })}
+                >
+                  {({ remainingTime }) => (
+                    <Animated.Text style={{ fontSize: 50 }}>
+                      {remainingTime}
+                    </Animated.Text>
+                  )}
+                </CountdownCircleTimer>
+                <Text style={styles.modalText}>
+                  Emergency message is sending...
+                </Text>
+
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => {
+                    setModalVisible(!modalVisible), setCancel(true);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Cancel</Text>
+                </Pressable>
+                <Text>If you're not sure click CANCEL</Text>
+              </View>
             </View>
-          </View>
-        </Modal>
+          </Modal>
 
-        <Pressable
-          style={[styles.emergencyButton]}
-          onPress={() => {
-            showModalTimer();
-          }}
-        >
-          <Text style={styles.emergencyMessage}>
-            In case of emergency click the button
-          </Text>
+          <Pressable
+            style={[styles.emergencyButton]}
+            onPress={() => {
+              showModalTimer(), sendLocation();
+            }}
+          >
+            <Text style={styles.emergencyMessage}>
+              In case of emergency click the button
+            </Text>
 
-          <EmergencyButtonUnpressed />
-        </Pressable>
-      </LinearGradient>
-    </View>
+            <EmergencyButtonUnpressed />
+          </Pressable>
+        </LinearGradient>
+      </View>
 
-    /* // <View style={styles.container}>
+      /* // <View style={styles.container}>
     //   <LinearGradient colors={mainGradient} style={styles.background}>
     //     <Pressable style={styles.emergencyButton}>
     //       <EmergencyButtonUnpressed />
     //     </Pressable>
     //   </LinearGradient>
     // </View> */
-  );
+    );
 
-  async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
+    async function registerForPushNotificationsAsync() {
+      let token;
+      if (Constants.isDevice) {
+        const { status: existingStatus } =
+          await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== "granted") {
+          const { status } = await Notifications.requestPermissionsAsync();
+          finalStatus = status;
+        }
+        if (finalStatus !== "granted") {
+          alert("Failed to get push token for push notification!");
+          return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+      } else {
+        alert("Must use physical device for Push Notifications");
       }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-    } else {
-      alert('Must use physical device for Push Notifications');
-    }
-  
-    if (Platform.OS === 'android') {
-      Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
-    }
-  
-    return token;
-  }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  background: {
-    height: windowHeight,
-    width: windowWidth,
-  },
-  text: {
-    color: "#000000",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  emergencyButton: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-  emergencyMessage: {
-    padding: 10,
-    flexDirection: "column",
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  emergencyButtonView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    elevation: 2,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+
+      return token;
+    }
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      alignItems: "center",
+      justifyContent: "center",
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-});
+    background: {
+      height: windowHeight,
+      width: windowWidth,
+    },
+    text: {
+      color: "#000000",
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    emergencyButton: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+    },
+    emergencyMessage: {
+      padding: 10,
+      flexDirection: "column",
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    emergencyButtonView: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      elevation: 2,
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22,
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2,
+    },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center",
+    },
+  });
+}
