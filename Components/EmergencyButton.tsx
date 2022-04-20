ea8f2fe3bb58c68dfeb73be98980d8996b96c71a
Michal <mincoslav@gmail.com>
2022-04-20 12:29:34 +0200

    indentation changes, typos fixed

--- Components/EmergencyButton.tsx
+++ Components/EmergencyButton.tsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import {
-  Pressable,
-  View,
-  Dimensions,
-  StyleSheet,
-  Text,
-  Modal,
-  Alert,
-  Animated,
-  Platform,
+	Pressable,
+	View,
+	Dimensions,
+	StyleSheet,
+	Text,
+	Modal,
+	Alert,
+	Animated,
+	Platform
 } from "react-native";
 import * as Location from "expo-location";
 import * as SecureStore from "expo-secure-store";
@@ -22,257 +22,262 @@ import Constants from "expo-constants";
 
 import { windowHeight, windowWidth, mainGradient } from "../css/styles";
 
-async function messagesentNotification() {
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: "your message has been sent",
-      body: "we'll notify you when someone responds",
-      data: { data: "goes here" },
-    },
-    trigger: null,
-  });
+async function messageSentNotification() {
+	await Notifications.scheduleNotificationAsync({
+		content: {
+			title: "your message has been sent",
+			body: "we'll notify you when someone responds",
+			data: { data: "goes here" }
+		},
+		trigger: null
+	});
 }
 
 export default function EmergencyButton() {
-  const [modalVisible, setModalVisible] = useState(false);
-  let cancel = false;
-  const uid = SecureStore.getItemAsync(userID);
-  const [latitude, setLatitude] = React.useState(0);
-  const [longitude, setLongitude] = React.useState(0);
-  const location = {
-    type: "Point",
-    coordinates: [longitude, latitude],
-    last_updated: Date.now(),
-    country: "denamrk",
-  };
-  const conenctToSendLocation = () => {
-    fetch("http://localhost:7071/create_alert/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        location: location,
-        userID: uid,
-        responderID: "",
-        resolved: false,
-        status: "received",
-      }),
-    })
-      .then((res) => {
-        cancel = false;
-        if (res.status === 201) {
-          console.log(res);
-          messagesentNotification();
-        }
-      })
-      .catch((err) => {
-        cancel = false;
-        console.log(err);
-      });
-  };
-  const sendLocation = () => {
-    
-    setTimeout(() => {
-      console.log(cancel);
-      if (cancel === false) {   
-        console.log("sending location");
-        conenctToSendLocation();  
-        cancel = false;     
-      }
-    }, 5000);
-  };
-  // const cancelLocation = () => {
-  //   cancel = true;
-  // }
-  const showModalTimer = () => {
-    setModalVisible(true);
-    setTimeout(() => {
-      setModalVisible(false);
-    }, 4000);
-  };
+	const [modalVisible, setModalVisible] = useState(false);
+	let cancel = false;
+	const uid = SecureStore.getItemAsync(userID);
+	const [latitude, setLatitude] = React.useState(0);
+	const [longitude, setLongitude] = React.useState(0);
+	const location = {
+		type: "Point",
+		coordinates: [longitude, latitude],
+		last_updated: Date.now(),
+		country: "denamrk"
+	};
+	
+	const connectToSendLocation = () => {
+		fetch("http://localhost:7071/create_alert/", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				location: location,
+				userID: uid,
+				responderID: "",
+				resolved: false,
+				status: "received"
+			})
+		})
+			.then((res) => {
+				cancel = false;
+				if (res.status === 201) {
+					console.log(res);
+					messageSentNotification();
+				}
+			})
+			.catch((err) => {
+				cancel = false;
+				console.log(err);
+			});
+	};
 
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        alert("Permission to access location was denied");
-        return;
-      }
-      let location = await Location.getCurrentPositionAsync({});
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
-    })();
-  }, []);
+	const sendLocation = () => {
+		setTimeout(() => {
+			console.log(cancel);
+			if (cancel === false) {
+				console.log("sending location");
+				connectToSendLocation();
+				cancel = false;
+			}
+		}, 5000);
+	};
+	
+	const showModalTimer = () => {
+		setModalVisible(true);
+		setTimeout(() => {
+			setModalVisible(false);
+		}, 4000);
+	};
 
-  return (
-    <View style={styles.container}>
-      <LinearGradient colors={mainGradient} style={styles.background}>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert("Emergency message was cancelled");
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <CountdownCircleTimer
-                isPlaying
-                duration={3}
-                colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-                colorsTime={[3, 2, 1, 0]}
-                onComplete={() => ({ shouldRepeat: false })}
-              >
-                {({ remainingTime }) => (
-                  <Animated.Text style={{ fontSize: 50 }}>
-                    {remainingTime}
-                  </Animated.Text>
-                )}
-              </CountdownCircleTimer>
-              <Text style={styles.modalText}>
-                Emergency message is sending...
-              </Text>
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				alert("Permission to access location was denied");
+				return;
+			}
+			let location = await Location.getCurrentPositionAsync({});
+			setLatitude(location.coords.latitude);
+			setLongitude(location.coords.longitude);
+		})();
+	}, []);
 
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => {
-                  cancel=true;
-                  //cancelLocation();
-                  console.log(cancel);
-                  setModalVisible(!modalVisible);
-                }}
-              >
-                <Text style={styles.textStyle}>Cancel</Text>
-              </Pressable>
-              <Text>If you're not sure click CANCEL</Text>
-            </View>
-          </View>
-        </Modal>
+	return (
+		<View style={styles.container}>
+			<LinearGradient colors={mainGradient} style={styles.background}>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						Alert.alert("Emergency message was cancelled");
+						setModalVisible(!modalVisible);
+					}}
+				>
+					<View style={styles.centeredView}>
+						<View style={styles.modalView}>
+							<CountdownCircleTimer
+								isPlaying
+								duration={3}
+								colors={[
+									"#004777",
+									"#F7B801",
+									"#A30000",
+									"#A30000"
+								]}
+								colorsTime={[3, 2, 1, 0]}
+								onComplete={() => ({ shouldRepeat: false })}
+							>
+								{({ remainingTime }) => (
+									<Animated.Text style={{ fontSize: 50 }}>
+										{remainingTime}
+									</Animated.Text>
+								)}
+							</CountdownCircleTimer>
+							<Text style={styles.modalText}>
+								Emergency message is sending...
+							</Text>
 
-        <Pressable
-          style={[styles.emergencyButton]}
-          onPress={() => {
-            showModalTimer()
-             sendLocation();
-          }}
-        >
-          <Text style={styles.emergencyMessage}>
-            In case of emergency click the button
-          </Text>
+							<Pressable
+								style={[styles.button, styles.buttonClose]}
+								onPress={() => {
+									cancel = true;
+									//cancelLocation();
+									console.log(cancel);
+									setModalVisible(!modalVisible);
+								}}
+							>
+								<Text style={styles.textStyle}>Cancel</Text>
+							</Pressable>
+							<Text>If you're not sure click CANCEL</Text>
+						</View>
+					</View>
+				</Modal>
 
-          <EmergencyButtonUnpressed />
-        </Pressable>
-      </LinearGradient>
-    </View>
-  );
+				<Pressable
+					style={[styles.emergencyButton]}
+					onPress={() => {
+						showModalTimer();
+						sendLocation();
+					}}
+				>
+					<Text style={styles.emergencyMessage}>
+						In case of emergency click the button
+					</Text>
 
-  async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-    } else {
-      alert("Must use physical device for Push Notifications");
-    }
+					<EmergencyButtonUnpressed />
+				</Pressable>
+			</LinearGradient>
+		</View>
+	);
 
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
+	async function registerForPushNotificationsAsync() {
+		let token;
+		if (Constants.isDevice) {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const { status } =
+					await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				alert("Failed to get push token for push notification!");
+				return;
+			}
+			token = (await Notifications.getExpoPushTokenAsync()).data;
+			console.log(token);
+		} else {
+			alert("Must use physical device for Push Notifications");
+		}
 
-    return token;
-  }
+		if (Platform.OS === "android") {
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C"
+			});
+		}
+
+		return token;
+	}
 }
 const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  background: {
-    height: windowHeight,
-    width: windowWidth,
-  },
-  text: {
-    color: "#000000",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  emergencyButton: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-  emergencyMessage: {
-    padding: 10,
-    flexDirection: "column",
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  emergencyButtonView: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-    elevation: 2,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "#F194FF",
-  },
-  buttonClose: {
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
+	container: {
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	background: {
+		height: windowHeight,
+		width: windowWidth
+	},
+	text: {
+		color: "#000000",
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	emergencyButton: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column"
+	},
+	emergencyMessage: {
+		padding: 10,
+		flexDirection: "column",
+		color: "white",
+		fontWeight: "bold",
+		textAlign: "center"
+	},
+	emergencyButtonView: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		flexDirection: "column",
+		elevation: 2
+	},
+	centeredView: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		marginTop: 22
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 35,
+		alignItems: "center",
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5
+	},
+	button: {
+		borderRadius: 20,
+		padding: 10,
+		elevation: 2
+	},
+	buttonOpen: {
+		backgroundColor: "#F194FF"
+	},
+	buttonClose: {
+		backgroundColor: "#2196F3"
+	},
+	textStyle: {
+		color: "white",
+		fontWeight: "bold",
+		textAlign: "center"
+	},
+	modalText: {
+		marginBottom: 15,
+		textAlign: "center"
+	}
 });
--- Components/MapViewComponent.tsx
+++ Components/MapViewComponent.tsx
@@ -9,151 +9,158 @@ import * as Notifications from "expo-notifications";
 import Constants from "expo-constants";
 
 Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: false,
+		shouldSetBadge: false
+	})
 });
 
 export default function MapViewComponent() {
-  const [token, setToken] = useState("");
-  const [notification, setNotification] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  const region = {
-    latitude: latitude,
-    longitude: longitude,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  };
-  //the geoLocation object obtained from geoJson
-  const dangerousArea = [
-    { latitude: 12.538511753082275, longitude: 55.8107594520295 },
-    { latitude: 12.538619041442871, longitude: 55.8138340359014 },
-    { latitude: 12.533115148544312, longitude: 55.81333970742995 },
-    { latitude: 12.534842491149902, longitude: 55.810946345433216 },
-    { latitude: 12.538511753082275, longitude: 55.8107594520295 },
-  ];
-  const polyObject = {
-    geometries: [],
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        properties: {},
-        geometry: {
-          type: "Polygon",
-          coordinates: [
-            [
-              [9.852354526519775, 55.8617359982937],
-              [9.853706359863281, 55.86159751652741],
-              [9.853438138961792, 55.86208521099058],
-              [9.85249400138855, 55.86209725275172],
-              [9.852354526519775, 55.8617359982937],
-            ],
-          ],
-        },
-      },
-    ],
-  };
-  const checkIfInDangerousArea = () => {
-    if (d3.geoContains(polyObject, [longitude+20, latitude+10])) {
-      schedulePushNotification();
-    }
-  };
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        alert("Permission to access location was denied");
-        return;
-      }
-      let location = await Location.getCurrentPositionAsync({});
-      setLatitude(location.coords.latitude);
-      setLongitude(location.coords.longitude);
-    })();
-    //this doesn't make any sense to me :)
-    registerForPushNotificationsAsync().then((token) => setToken(token));
+	const [token, setToken] = useState("");
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
+	const [latitude, setLatitude] = useState(0);
+	const [longitude, setLongitude] = useState(0);
+	const region = {
+		latitude: latitude,
+		longitude: longitude,
+		latitudeDelta: 0.0922,
+		longitudeDelta: 0.0421
+	};
+	//the geoLocation object obtained from geoJson
+	const dangerousArea = [
+		{ latitude: 12.538511753082275, longitude: 55.8107594520295 },
+		{ latitude: 12.538619041442871, longitude: 55.8138340359014 },
+		{ latitude: 12.533115148544312, longitude: 55.81333970742995 },
+		{ latitude: 12.534842491149902, longitude: 55.810946345433216 },
+		{ latitude: 12.538511753082275, longitude: 55.8107594520295 }
+	];
+	const polyObject = {
+		geometries: [],
+		type: "FeatureCollection",
+		features: [
+			{
+				type: "Feature",
+				properties: {},
+				geometry: {
+					type: "Polygon",
+					coordinates: [
+						[
+							[9.852354526519775, 55.8617359982937],
+							[9.853706359863281, 55.86159751652741],
+							[9.853438138961792, 55.86208521099058],
+							[9.85249400138855, 55.86209725275172],
+							[9.852354526519775, 55.8617359982937]
+						]
+					]
+				}
+			}
+		]
+	};
+	const checkIfInDangerousArea = () => {
+		if (d3.geoContains(polyObject, [longitude + 20, latitude + 10])) {
+			schedulePushNotification();
+		}
+	};
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				alert("Permission to access location was denied");
+				return;
+			}
+			let location = await Location.getCurrentPositionAsync({});
+			setLatitude(location.coords.latitude);
+			setLongitude(location.coords.longitude);
+		})();
+		//this doesn't make any sense to me :)
+		registerForPushNotificationsAsync().then((token) => setToken(token));
 
-    notificationListener.current =
-      Notifications.addNotificationReceivedListener((notification) =>
-        setNotification(notification)
-      );
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) =>
+				setNotification(notification)
+			);
 
-    responseListener.current =
-      Notifications.addNotificationResponseReceivedListener((response) => {
-        console.log(response);
-      });
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener(
+				(response) => {
+					console.log(response);
+				}
+			);
 
-    return () => {
-      Notifications.removeNotificationSubscription(
-        notificationListener.current
-      );
-      Notifications.removeNotificationSubscription(responseListener.current);
-    };
-  }, []);
+		return () => {
+			Notifications.removeNotificationSubscription(
+				notificationListener.current
+			);
+			Notifications.removeNotificationSubscription(
+				responseListener.current
+			);
+		};
+	}, []);
 
-  return (
-    <View style={styles.container}>
-      <LinearGradient colors={mainGradient} style={styles.background}>
-        <View style={styles.container}>
-          <MapView style={styles.map} showsUserLocation={true} region={region}>
-            <Polygon coordinates={dangerousArea} />
-          </MapView>
-          <Pressable
-            onPress={checkIfInDangerousArea} 
-          >
-            <Text>Press me to check if you are in a dangerous area</Text> 
-          </Pressable>
-        
-          
-        </View>
-      </LinearGradient>
-    </View>
-  );
-  async function schedulePushNotification() {
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "Dangerous Area",
-        body: "You have entered a dangerous area!",
-        data: { data: "goes here" },
-      },
-      trigger: null,
-    });
-  }
+	return (
+		<View style={styles.container}>
+			<LinearGradient colors={mainGradient} style={styles.background}>
+				<View style={styles.container}>
+					<MapView
+						style={styles.map}
+						showsUserLocation={true}
+						region={region}
+					>
+						<Polygon coordinates={dangerousArea} />
+					</MapView>
+					<Pressable onPress={checkIfInDangerousArea}>
+						<Text>
+							Press me to check if you are in a dangerous area
+						</Text>
+					</Pressable>
+				</View>
+			</LinearGradient>
+		</View>
+	);
+	async function schedulePushNotification() {
+		await Notifications.scheduleNotificationAsync({
+			content: {
+				title: "Dangerous Area",
+				body: "You have entered a dangerous area!",
+				data: { data: "goes here" }
+			},
+			trigger: null
+		});
+	}
 
-  async function registerForPushNotificationsAsync() {
-    let token;
-    if (Constants.isDevice) {
-      const { status: existingStatus } =
-        await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== "granted") {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== "granted") {
-        alert("Failed to get push token for push notification!");
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
-    } else {
-      alert("Must use physical device for Push Notifications");
-    }
+	async function registerForPushNotificationsAsync() {
+		let token;
+		if (Constants.isDevice) {
+			const { status: existingStatus } =
+				await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== "granted") {
+				const { status } =
+					await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== "granted") {
+				alert("Failed to get push token for push notification!");
+				return;
+			}
+			token = (await Notifications.getExpoPushTokenAsync()).data;
+			console.log(token);
+		} else {
+			alert("Must use physical device for Push Notifications");
+		}
 
-    if (Platform.OS === "android") {
-      Notifications.setNotificationChannelAsync("default", {
-        name: "default",
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      });
-    }
+		if (Platform.OS === "android") {
+			Notifications.setNotificationChannelAsync("default", {
+				name: "default",
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: "#FF231F7C"
+			});
+		}
 
-    return token;
-  }
+		return token;
+	}
 }
