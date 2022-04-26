e0d5aa9b7e9733aff99eb0c8bc37e44c0304840e
Michal <mincoslav@gmail.com>
2022-04-26 11:10:47 +0200

    top bar fixed

--- Components/Navigation/MainContainer.tsx
+++ Components/Navigation/MainContainer.tsx
@@ -1,3 +1,4 @@
+import { StatusBar } from "expo-status-bar";
 import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,7 +8,14 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 // Importing Pages
 import EmergencyButtonPage from "./Screens/EmergencyButtonScreen";
 import MapScreen from "./Screens/MapScreen";
-import { Pressable, View, Text } from "react-native";
+import {
+	StyleSheet,
+	Pressable,
+	View,
+	Text,
+	SafeAreaView,
+	Platform
+} from "react-native";
 import { AuthContext } from "../../App";
 
 const Tab = createBottomTabNavigator();
@@ -17,51 +25,73 @@ export default function MainContainer() {
 
 	return (
 		<NavigationContainer independent={true}>
-			<Tab.Navigator
-				initialRouteName="Emergency"
-				screenOptions={{
-					tabBarActiveTintColor: "#34AAFC"
-				}}
-			>
-				<Tab.Screen
-					name="Emergency"
-					component={EmergencyButtonPage}
-					options={{
-						tabBarLabel: "Emergency",
-						tabBarStyle: { position: "absolute" },
-						tabBarIcon: ({ color, size }) => (
-							<Ionicons name="shield" color={color} size={size} />
-						),
-						headerTransparent: true,
-						headerTitleAlign: "center",
-						headerRight: () => (
-							<Pressable
-								onPress={() => signOut()}
-							>
-								<View>
-									<Text>Sign Out</Text>
-								</View>
-							</Pressable>
-						)
+			<StatusBar style="auto" />
+			<SafeAreaView style={styles.topContainer} />
+			<SafeAreaView style={styles.mainContainer}>
+				<Tab.Navigator
+					initialRouteName="Emergency"
+					screenOptions={{
+						tabBarActiveTintColor: "#34AAFC"
 					}}
-				/>
-				<Tab.Screen
-					name="Map"
-					component={MapScreen}
-					options={{
-						tabBarLabel: "Map",
-						tabBarIcon: ({ color, size }) => (
-							<Ionicons
-								name="location"
-								color={color}
-								size={size}
-							/>
-						),
-						headerTransparent: true,
-						headerTitleAlign: "center"
-					}}
-				/>
-			</Tab.Navigator>
+				>
+					<Tab.Screen
+						name="Emergency"
+						component={EmergencyButtonPage}
+						options={{
+							tabBarLabel: "Emergency",
+							tabBarStyle: { position: "absolute" },
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons
+									name="shield"
+									color={color}
+									size={size}
+								/>
+							),
+							headerTransparent: true,
+							headerTitleAlign: "center",
+							headerRight: () => (
+								<Pressable onPress={() => signOut()} style={{marginRight:10}}>
+									<View>
+										<Text>Sign Out</Text>
+									</View>
+								</Pressable>
+							)
+						}}
+					/>
+					<Tab.Screen
+						name="Map"
+						component={MapScreen}
+						options={{
+							tabBarLabel: "Map",
+							tabBarIcon: ({ color, size }) => (
+								<Ionicons
+									name="location"
+									color={color}
+									size={size}
+								/>
+							),
+							headerTransparent: true,
+							headerTitleAlign: "center"
+						}}
+					/>
+				</Tab.Navigator>
+			</SafeAreaView>
 		</NavigationContainer>
 	);
 }
+
+const styles = StyleSheet.create({
+	// Sets the color at the top of "SafeAreaView"
+	topContainer: {
+		flex: 0,
+		backgroundColor: "#34AAFC",
+		paddingTop: Platform.OS === "android" ? 30 : 0
+	},
+	// Sets the color at the bottom of "SafeAreaView"
+	mainContainer: {
+		flex: 1,
+		// backgroundColor: "#760ACA",
+		backgroundColor: "red"
+		// paddingTop: Platform.OS === 'android' ? 25 : 0
+	}
+});
